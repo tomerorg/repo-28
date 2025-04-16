5bc9f9d12680e8e3ffe2504abf579346fbd8ab20
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:55:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-28",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental analytics project using NumPy and Django in Python",
+    keywords="repo-28, python",
+    url="",
+)
